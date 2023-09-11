44ef314a919ff24d7ee57fe4f233d69e766ff0fc
RGGG1 <>
2023-09-11 19:27:59 +0000

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,11 +17,11 @@ function choosePic() {
             myImages1[3] = "assets/3.png";
             myImages1[4] = "assets/4.png";
             myImages1[5] = "assets/5.png";
-            myImages1[2] = "assets/6.png";
-            myImages1[3] = "assets/7.png";
-            myImages1[4] = "assets/8.png";
-            myImages1[5] = "assets/9.png";
-            myImages1[5] = "assets/10.png";
+            myImages1[6] = "assets/6.png";
+            myImages1[7] = "assets/7.png";
+            myImages1[8] = "assets/8.png";
+            myImages1[9] = "assets/9.png";
+            myImages1[10] = "assets/10.png";
           var rnd = Math.floor(Math.random() * myImages1.length);
             if (rnd == 0) {
                     rnd = 1;
