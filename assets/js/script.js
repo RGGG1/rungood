fdebfad6fbdc33a8e4dbbb6c585a4cbc2df68662
RGGG1 <>
2023-09-11 00:23:52 +0000

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,11 @@ function imgchange() {
         myImages1[3] = "assets/3.png";
         myImages1[4] = "assets/4.png";
         myImages1[5] = "assets/5.png";
+        myImages1[2] = "assets/6.png";
+        myImages1[3] = "assets/7.png";
+        myImages1[4] = "assets/8.png";
+        myImages1[5] = "assets/9.png";
+        myImages1[5] = "assets/10.png";
       var rnd = Math.floor(Math.random() * myImages1.length);
         if (rnd == 0) {
                 rnd = 1;
