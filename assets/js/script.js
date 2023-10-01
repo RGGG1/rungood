0d1b66cc87b4ed4274d65ff430419d5c8ef5b361
RGGG1 <>
2023-10-01 17:40:49 +0000

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -171,3 +171,38 @@ function imgchangedraw1() {
             }
 
 
+/* Anybet */
+
+function betWin()
+{
+const bet = document.getElementById("bet");
+console.log(bet.value)
+
+const win = document.getElementById("win");
+console.log(win.value)
+
+const oddsTotal = parseFloat(bet.value) + parseFloat(win.value);
+console.log(oddsTotal)
+
+const playerWinChance = ((parseFloat(bet.value) / oddsTotal)*100);
+console.log(playerWinChance)
+
+const playerNumber = (playerWinChance * oddsTotal);
+console.log(playerNumber)
+
+const min = 0;
+const max = oddsTotal;
+winningNumber = Math.round(Math.random() * (max - min) + (min));
+console.log(winningNumber);
+
+if (playerNumber <= winningNumber) {
+    outcome = "You Win";
+    
+
+  } else {
+    outcome = "You Lose";
+}
+
+console.log(outcome)
+}
+
